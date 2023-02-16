7fcfa288fe2eec15b4821c44c44595e536443c9d
ТрифоноваАР <nastytrifono03@gmail.com>
2023-02-16 10:46:08 +0300

    Тест

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -103,6 +103,20 @@ namespace Test
             double actual = Class1.MaterialFromMarriage(count, witdh, height, poductType, materialType);
             Assert.IsFalse(actual==-1);
         }
+
+        [TestMethod]
+        //Проверяет что при вводе не всех правильных значений метод  возратит результат -1
+        public void NotCorrectResultIsWrong()
+        {
+
+            int poductType = 4;
+            int materialType = 2;
+            int count = 4;
+            double witdh = 11;
+            double height = 30;
+            double actual = Class1.MaterialFromMarriage(count, witdh, height, poductType, materialType);
+            Assert.IsTrue(actual == -1);
+        }
         [TestMethod]
         //Проверяет что результат не выходит за пределы double
         public void CorrectResultTypeMAxAndMinValue()
@@ -114,7 +128,95 @@ namespace Test
             double witdh = 2000;
             double height = 5000;
             double actual = Class1.MaterialFromMarriage(count, witdh, height, poductType, materialType);
-            Assert.IsTrue(actual > (-1.79*Math.Pow(10,308)) && (1.79 * Math.Pow(10, 308)) < actual);
+            Assert.IsTrue(actual > -9223372036854775808 && actual< 9223372036854775808);
+        }
+
+        [TestMethod]
+        //Проверяет что результат не округляется в большую сторону
+        public void TheResultIsRoundedUp()
+        {
+
+            int poductType = 1;
+            int materialType = 1;
+            int count = 10;
+            double witdh = 5;
+            double height = 25;
+            double except = 1380; //1379.125
+            double actual = Class1.MaterialFromMarriage(count, witdh, height, poductType, materialType);
+            Assert.AreNotEqual(except, actual);
+        }
+        //Сложные тесты
+        [TestMethod]
+        //Проверяет что при вводе самых больших чисел ответ будет корректный
+        public void CorrecteRsultForLargeNumbers()
+        {
+
+            int poductType = 3;
+            int materialType = 1;
+            int count = 999999999;
+            double witdh = 9999999999999999999;
+            double height = 9999999999999999999;
+            double actual = Class1.MaterialFromMarriage(count, witdh, height, poductType, materialType);
+            Assert.IsFalse(actual ==-1);
+        }
+
+        [TestMethod]
+        //Проверяет что при вводе самых маленьки чисел ответ будет корректный
+        public void CorrecteRsultForSmallNumbers()
+        {
+
+            int poductType = 3;
+            int materialType = 1;
+            int count = 0;
+            double witdh = 0;
+            double height = 0;
+            double actual = Class1.MaterialFromMarriage(count, witdh, height, poductType, materialType);
+            Assert.AreEqual(0,actual);
+        }
+
+        [TestMethod]
+        //Проверка на не существующий тип данных продукции
+        public void Non_existintProductDataType()
+        {
+
+            int poductType = 421;
+            int materialType = 1;
+            int count = 20;
+            double witdh = 65;
+            double height = 120;
+            double except = -1;
+            double actual = Class1.MaterialFromMarriage(count, witdh, height, poductType, materialType);
+            Assert.AreEqual(except, actual);
+        }
+
+        [TestMethod]
+        //Проверка на не существующий тип данных материла
+        public void Non_existintMaterialDataType()
+        {
+
+            int poductType = 1;
+            int materialType = 102;
+            int count = 20;
+            double witdh = 65;
+            double height = 120;
+            double except = -1;
+            double actual = Class1.MaterialFromMarriage(count, witdh, height, poductType, materialType);
+            Assert.AreEqual(except, actual);
+        }
+
+        [TestMethod]
+        //Проверка на отрицательное число 
+        public void NegativeNumber()
+        {
+
+            int poductType = 1;
+            int materialType = 2;
+            int count = -20;
+            double witdh = 65;
+            double height = 120;
+            double except = -1;
+            double actual = Class1.MaterialFromMarriage(count, witdh, height, poductType, materialType);
+            Assert.AreEqual(except, actual);
         }
     }
 }
--- WSUniversalLib/Class1.cs
+++ WSUniversalLib/Class1.cs
@@ -14,10 +14,10 @@ namespace WSUniversalLib
         double height;
         double product_type;
         double material_type;
-      public static  double  kolvo_mater;
+        public static double kolvo_mater;
         public static double kolvo_material_without_marriage;
 
-        public Class1(int count, double width,double height, double product_type, double material_type)
+        public Class1(int count, double width, double height, double product_type, double material_type)
         {
             this.count = count;
             this.width = width;
@@ -26,67 +26,80 @@ namespace WSUniversalLib
             this.material_type = material_type;
         }
 
-        public static double MaterialFromMarriage (int count, double width, double height, double product_type, double material_type)
+        public static double MaterialFromMarriage(int count, double width, double height, double product_type, double material_type)
         {
-            switch(product_type)
-            {
-                case 1:
-                    kolvo_mater = width * height * count * 1.1;
-                    switch (material_type)
-                    {
-                        case 1:
-                            kolvo_material_without_marriage = kolvo_mater / 100 * 100.3;
-                            return kolvo_material_without_marriage;
-                            break;
-                        case 2:
-                            kolvo_material_without_marriage = kolvo_mater / 100 * 100.12;
-                            return kolvo_material_without_marriage;
-                            break;
-                        default:
-                            return -1;
-                            break;
-                    }
-                    break;
-                case 2:
-                    kolvo_mater = width * height * count * 2.5;
-                    switch (material_type)
-                    {
-                        case 1:
-                            kolvo_material_without_marriage = kolvo_mater / 100 * 100.3;
-                            return kolvo_material_without_marriage;
-                            break;
-                        case 2:
-                            kolvo_material_without_marriage = kolvo_mater / 100 * 100.12;
-                            return kolvo_material_without_marriage;
-                            break;
-                        default:
-                            return -1;
-                            break;
-                    }
-                    break;
-                case 3:
-                    kolvo_mater = width * height * count * 8.43;
-                    switch (material_type)
-                    {
-                        case 1:
-                            kolvo_material_without_marriage = kolvo_mater / 100 * 100.3;
-                            return kolvo_material_without_marriage;
-                            break;
-                        case 2:
-                            kolvo_material_without_marriage = kolvo_mater / 100 * 100.12;
-                            return kolvo_material_without_marriage;
-                            break;
-                        default:
-                            return -1;
-                            break;
-                    }
-                    break;
+           
+           
+                if ((count <0 )||( width < 0) || (height< 0))
+                {
+                return -1;
+                }
+                else
+                {
+                switch (product_type)
+                {
+                    case 1:
+                        kolvo_mater = width * height * count * 1.1;
+                        switch (material_type)
+                        {
+                            case 1:
+                                kolvo_material_without_marriage = kolvo_mater / 100 * 100.3;
+                                return kolvo_material_without_marriage;
+                                break;
+                            case 2:
+                                kolvo_material_without_marriage = kolvo_mater / 100 * 100.12;
+                                return kolvo_material_without_marriage;
+                                break;
+                            default:
+                                return -1;
+                                break;
+                        }
+                        break;
+                    case 2:
+                        kolvo_mater = width * height * count * 2.5;
+                        switch (material_type)
+                        {
+                            case 1:
+                                kolvo_material_without_marriage = kolvo_mater / 100 * 100.3;
+                                return kolvo_material_without_marriage;
+                                break;
+                            case 2:
+                                kolvo_material_without_marriage = kolvo_mater / 100 * 100.12;
+                                return kolvo_material_without_marriage;
+                                break;
+                            default:
+                                return -1;
+                                break;
+                        }
+                        break;
+                    case 3:
+                        kolvo_mater = width * height * count * 8.43;
+                        switch (material_type)
+                        {
+                            case 1:
+                                kolvo_material_without_marriage = kolvo_mater / 100 * 100.3;
+                                return kolvo_material_without_marriage;
+                                break;
+                            case 2:
+                                kolvo_material_without_marriage = kolvo_mater / 100 * 100.12;
+                                return kolvo_material_without_marriage;
+                                break;
+                            default:
+                                return -1;
+                                break;
+                        }
+                        break;
                     default:
-                    return -1;
-                    break;
+                        return -1;
+                        break;
+                }
             }
-        }
-      
+            
+            
         
+        }
     }
+
+
 }
+
